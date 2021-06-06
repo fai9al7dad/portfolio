721123f3e7148535eae2588f51374efcfe4e029f
unknown <fai9al7dad@gmail.com>
2021-06-06 20:10:15 +0300

    LS attempt 7

--- src/hook/useDarkMode.js
+++ src/hook/useDarkMode.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
+const isBrowser = typeof window !== "undefined"
 export default function useDarkMode() {
-
-    const [theme,setTheme] = useState(null)
-    const colorTheme = theme === 'dark' ? 'light' :'dark'
-    useEffect(() =>{
-        const root = window.document.documentElement;
-
-        root.classList.remove(colorTheme)
-        root.classList.add(theme)
-        if(typeof window !== 'undefined'){
+    if(isBrowser) {
+        const [theme,setTheme] = useState(localStorage.theme)
+        const colorTheme = theme === 'dark' ? 'light' :'dark'
+        useEffect(() =>{
+            const root = window.document.documentElement;
+    
+            root.classList.remove(colorTheme)
+            root.classList.add(theme)
             localStorage.setItem('theme', theme)
-            setTheme(localstorage.theme)
-        } 
-    },[colorTheme,setTheme])
+        },[colorTheme,setTheme])
+    
+        return [colorTheme,setTheme]
 
-    return [colorTheme,setTheme]
+    }
 }
