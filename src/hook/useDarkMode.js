d7d9f1cc8ec147f67d86201bc32e876b4aa1f932
unknown <fai9al7dad@gmail.com>
2021-06-06 15:54:07 +0300

    first true comit

--- src/hook/useDarkMode.js
+++ src/hook/useDarkMode.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
 export default function useDarkMode() {
-    const [theme,setTheme] = useState(localStorage.theme)
+    const [theme,setTheme] = useState('dark')
     const colorTheme = theme === 'dark' ? 'light' :'dark'
     useEffect(() =>{
         const root = window.document.documentElement;
 
         root.classList.remove(colorTheme)
         root.classList.add(theme)
-        localStorage.setItem('theme', theme)
     },[colorTheme,setTheme])
 
     return [colorTheme,setTheme]
