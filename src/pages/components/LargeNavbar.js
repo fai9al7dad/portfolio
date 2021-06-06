77a978e94aa81f785e124e57016f8568dfd18317
unknown <fai9al7dad@gmail.com>
2021-06-06 21:25:21 +0300

    LS attempt 12 completed

--- src/pages/components/LargeNavbar.js
+++ src/pages/components/LargeNavbar.js
@@ -15,7 +15,7 @@ export default function LargeNavbar() {
 
             <div className=" mr-20 text-white" onClick={() => setTheme(colorTheme)}>
               {colorTheme === 'dark' ? (
-                <svg className="w-6 h-6 text-white rounded-full bg-dark-darkBlue cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" /></svg>
+                <svg className="w-6 h-6 text-dark-darkblue rounded-full bg-dark-darkBlue cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" /></svg>
               
                ) : (
                 <svg className="w-6 h-6 text-white rounded-full bg-light-lightBG cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" /></svg>
