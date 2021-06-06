d71ab1cc03ee289bc81094ea10eb74a9abbae249
unknown <fai9al7dad@gmail.com>
2021-06-06 20:27:18 +0300

    LS attempt 9

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import useDarkMode from '../../hook/useDarkMode';
 import Footer from './Footer'
 import LargeNavbar from './LargeNavbar'
 import MobileNavbar from './MobileNavbar'
 export default function Layout({children}) {
-    useDarkMode();
-
     return (
         <div>
             <LargeNavbar/>
