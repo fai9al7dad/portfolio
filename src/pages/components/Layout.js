55be80928bad133b843f847f88a9f6bf7997a1b0
unknown <fai9al7dad@gmail.com>
2021-06-08 11:01:13 +0300

    seo

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -3,11 +3,23 @@ import useDarkMode from '../../hook/useDarkMode';
 import Footer from './Footer'
 import LargeNavbar from './LargeNavbar'
 import MobileNavbar from './MobileNavbar'
+import { Helmet } from "react-helmet"
+
 export default function Layout({children}) {
     useDarkMode();
 
     return (
         <div>
+             <Helmet>
+                <title>فيصل حداد | Faisal haddad</title>
+                <meta name="og:title" content="فيصل حداد | Faisal haddad"/>
+                <meta name="description" content="مبرمج ومصمم مواقع وتطبيقات، أساعدك في وضع أفكارك وطموحاتك على العالم التقني"/>
+                <meta name="og:description" content="مبرمج ومصمم مواقع وتطبيقات، أساعدك في وضع أفكارك وطموحاتك على العالم التقني"/>
+                <link rel="canonical" href="https://faisalhaddad.com" />
+                <meta charSet="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Helmet>
             <LargeNavbar/>
             <div>
                 {children}
