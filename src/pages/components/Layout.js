ea9a921648ed465467d14e401d7e77d87e68fe77
unknown <fai9al7dad@gmail.com>
2021-06-09 06:28:14 +0300

    favicon

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -19,7 +19,7 @@ export default function Layout({children}) {
                 <meta charSet="utf-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="icon" href="../../../static/favicon.ico"></link>
+                <link rel="icon" href="../../images/favicon.ico"></link>
             </Helmet>
             <LargeNavbar/>
             <div>
